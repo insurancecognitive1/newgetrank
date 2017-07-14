003b6b37e146490827b9a5209e4b4f320dccb594
insurancecognitive1 <insurancecognitive1@gmail.com>
2017-07-14 14:54:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,15 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.send("Hello World!")
+app.get('/', function(request, res) {
+  //response.send("Hello World!")
+  var response = "This is a sample response from your webhook!" //Default response from the webhook to show it's working;
+
+res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
+  res.send(JSON.stringify({ "speech": response, "displayText": response 
+  //"speech" is the spoken version of the response, "displayText" is the visual version
+  }));
+  
 });
 
 app.listen(app.get('port'), function() {
