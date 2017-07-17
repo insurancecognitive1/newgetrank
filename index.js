89a4faaf8dd0e67fb75ffd600786423489815b2d
insurancecognitive1 <insurancecognitive1@gmail.com>
2017-07-17 14:02:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.post('/', function(request, res) {
   solrClient = retrieve_and_rank.createSolrClient(params);
   var query = solrClient.createQuery();
   query.q("how to add a new policy");
-  console.log("log request"+ request.body.result.parameters['question']);
+  console.log('log request'+ request.body.result);
   
   solrClient.search(query, function(err, searchResponse) {
   if(err) {
