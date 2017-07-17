9f93bd551d14c26fd53572f06c897eaaf34b476e
insurancecognitive1 <insurancecognitive1@gmail.com>
2017-07-17 14:30:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ var params = {
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
 app.post('/', function(req, res) {
   //response.send("Hello World!")
