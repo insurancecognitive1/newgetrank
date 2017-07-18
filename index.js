6726f9e02e6b4228ae7a49c72fe4dc2b844bce5c
insurancecognitive1 <insurancecognitive1@gmail.com>
2017-07-18 14:46:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.post('/', function(req, res) {
 //      res.send(JSON.stringify(resp));
         //res.send(searchResponse.response.docs[0]);
       res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
-  res.send(JSON.stringify({ "speech": response, "displayText": response ,"data": {"skype": {"text":response}}}));
+  res.send(JSON.stringify({ "speech": response, "displayText": response ,"data": {"skype": {"text":JSON.parse(JSON.stringify(searchResponse.response.docs[0].contentHtml, null, 2))}}}));
   //    res.send(JSON.stringify({ "speech": response, "displayText": response }));
     }
 });
